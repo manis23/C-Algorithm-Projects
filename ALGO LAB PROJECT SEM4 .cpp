8761b6deafd43fe842e25db7138fe5661060e9db
Manas Nishad <manasnishad346@gmail.com>
2019-05-13 14:37:03 +0530

    Add files via upload

---
+++ ALGO LAB PROJECT SEM4 .cpp
@@ -0,0 +1,384 @@
+#include <bits/stdc++.h>
+#include <cstdlib>
+#include <string.h>
+#include<windows.h>
+#include<conio.h>
+using namespace std;
+void gotoxy(int x, int y)
+{
+ COORD coord;
+ coord.X = x;
+ coord.Y = y;
+ SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+int funfram()
+{
+    gotoxy(0,0);
+    printf("\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2");
+    printf("\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2");
+    printf("\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2");
+    printf("\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2");
+    gotoxy(0,26);
+    printf("\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2");
+    printf("\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2");
+    printf("\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2\xDB\xDB\xB2\xB2");
+    printf("\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2\xDB\xB2");
+}
+
+string pattern[3]={"abc","def","ijk"};
+string pattern3[8]={"AM 09:00     ","AM 10:00     ","AM 11:00     ","AM 12:00     ","PM 01:00     ","PM 02:00     ","PM 03:00     ","PM 04:00     "};
+string TimeofPlane[8]={"AM 9:17     ","AM 10:47     ","AM 11:07     ","PM 01:47     ","PM 05:37     ","PM 12:47     ","PM 02:47     ","PM 03:08     "};
+int Num=8;
+
+class plane
+{
+public:
+    string patternOfPlane;
+    string name;
+    string time;
+    plane()
+    {
+        name="";
+        patternOfPlane="";
+        time="";
+    }
+};
+
+class runway
+{
+public:
+    int *lps;
+    int *indexOfRunway;
+    string patternOfRunway;
+    runway()
+    {
+        patternOfRunway="";
+        indexOfRunway=new int[Num];
+        for(int i=0;i<Num;i++)
+        {
+            indexOfRunway[i]=0;
+        }
+    }
+    void setlps(int M)
+    {
+        lps=new int[M];
+    }
+};
+
+void computeLPSArray(char* pat, int M, int* lps);
+
+// Prints occurrences of txt[] in pat[]
+/*void KMPSearch(char* pat, char* txt)
+{
+    int M = strlen(pat);
+    int N = strlen(txt);
+
+    // create lps[] that will hold the longest prefix suffix
+    // values for pattern
+    int lps[M];
+
+    // Preprocess the pattern (calculate lps[] array)
+    computeLPSArray(pat, M, lps);
+
+    int i = 0; // index for txt[]
+    int j = 0; // index for pat[]
+    while (i < N) {
+        if (pat[j] == txt[i]) {
+            j++;
+            i++;
+        }
+
+        if (j == M) {
+            printf("Found pattern at index %d ", i - j);
+            j = lps[j - 1];
+        }
+
+        // mismatch after j matches
+        else if (i < N && pat[j] != txt[i]) {
+            // Do not match lps[0..lps[j-1]] characters,
+            // they will match anyway
+            if (j != 0)
+                j = lps[j - 1];
+            else
+                i = i + 1;
+        }
+    }
+}*/
+
+// Fills lps[] for given patttern pat[0..M-1]
+void computeLPSArray(string pat, int M, int* lps)
+{
+    // length of the previous longest prefix suffix
+    int len = 0;
+
+    lps[0] = 0; // lps[0] is always 0
+
+    // the loop calculates lps[i] for i = 1 to M-1
+    int i = 1;
+    while (i < M) {
+        if (pat[i] == pat[len]) {
+            len++;
+            lps[i] = len;
+            i++;
+        }
+        else // (pat[i] != pat[len])
+        {
+            // This is tricky. Consider the example.
+            // AAACAAAA and i = 7. The idea is similar
+            // to search step.
+            if (len != 0) {
+                len = lps[len - 1];
+
+                // Also, note that we do not increment
+                // i here
+            }
+            else // if (len == 0)
+            {
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+}
+
+void Function1(plane p[],runway r[])
+{
+    for(int i=1;i<Num-1;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            if(r[j].indexOfRunway[i]==1 && r[j].indexOfRunway[i-1]==1)
+            {
+                for(int k=1;k<3;k++)
+                {
+                    if(!r[(j+k)%3].indexOfRunway[i+1])
+                    {
+                        r[(j+k)%3].indexOfRunway[i]=1;
+                        break;
+                    }
+                }
+                r[j].indexOfRunway[i]=0;
+            }
+        }
+    }
+    for(int j=0;j<3;j++)
+    {
+        if(r[j].indexOfRunway[Num-1]==1 && r[j].indexOfRunway[Num-2]==1)
+        {
+            r[(j+1)%3].indexOfRunway[Num-1]=1;
+            r[j].indexOfRunway[Num-1]=0;
+            break;
+        }
+    }
+    gotoxy(40,14);
+    cout<<"After Synchronising the Arrival Schedule"<<endl;
+    cout<<endl;
+    gotoxy(0,17);
+    cout<<"Alloted Time: ";
+    for(int x=0;x<Num;x++)
+    {
+        cout<<x+1<<"AM         ";
+    }
+    cout<<endl;
+    cout<<endl;
+    for(int i=0;i<3;i++)
+    {
+        gotoxy(0,17+i+1);
+        cout<<"Runway No "<<i+1<<" :";
+        for(int  j=0;j<Num;j++)
+        {
+            if(r[i].indexOfRunway[j])
+                cout << p[j].name;
+            else
+                cout << "free        ";
+        }
+        cout  <<  endl;
+    }
+}
+
+int Function(plane p[],runway r[],string arrival)
+{
+    for (int k=0;k<3;k++)
+    {
+        string pat=r[k].patternOfRunway;
+        int M = pat.length();
+        int N = arrival.length();
+
+        // create lps[] that will hold the longest prefix suffix
+        // values for pattern
+        r[k].setlps(M);
+        // Preprocess the pattern (calculate lps[] array)
+        computeLPSArray(pat, M, r[k].lps);
+        int i = 0; // index for txt[]
+        int j = 0; // index for pat[]
+        while (i < N) {
+            if (pat[j] == arrival[i]) {
+                j++;
+                i++;
+            }
+            if (j == M) {
+                r[k].indexOfRunway[i/3-1] =1;
+                j = r[k].lps[j - 1];
+            }
+            // mismatch after j matches
+            else if (i < N && pat[j] != arrival[i]) {
+                // Do not match lps[0..lps[j-1]] characters,
+                // they will match anyway
+                if (j != 0)
+                    j = r[k].lps[j - 1];
+                else
+                    i = i + 1;
+            }
+        }
+    }
+    gotoxy(0,8);
+    cout<<"Alloted Time: ";
+    for(int x=0;x<Num;x++)
+    {
+        cout<<x+1<<"AM         ";
+    }
+    cout<<endl;
+    cout<<endl;
+    for(int i=0;i<3;i++)
+    {
+        gotoxy(0,8+i+1);
+        cout<<"Runway No "<<i+1<<" :";
+        for(int  j=0;j<Num;j++)
+        {
+            if(r[i].indexOfRunway[j])
+                cout << p[j].name;
+            else
+                cout << "free        ";
+        }
+        cout  <<  endl;
+    }
+    cout << endl;
+    Function1(p,r);
+}
+int loading()
+{
+    int r,q;
+    gotoxy(40,18);
+    printf("Loading Please Wait\n");
+    gotoxy(35,18);
+    for(r=1;r<=30;r++)
+        {
+            for(q=0;q<=30000000;q++);  // helps in displaying the characters slowly and speed can be decreased by increasing the number of 0s ..
+            {
+                printf("");
+            }
+            printf("\xB2");
+        }
+}
+
+void intro()
+{
+    system("color 4F");
+    funfram();
+    gotoxy(40,6);
+    cout<<"\xB2\xDB  RUNWAY SCHEDULING SYSTEM  ";
+    gotoxy(45,8);
+    cout<<"\xB2\xDB MANAS NISHAD ";
+    gotoxy(45,10);
+    cout<<"\xDB\xB2 MRIDUL KUMAR ";
+    gotoxy(45,12);
+    cout<<"\xB2\xDB SHIVA KANSAL";
+    gotoxy(45,14);
+    cout<<"\xDB\xB2 HIMANSHU GUPTA";
+    gotoxy(40,16);
+    cout<<"PRESS ANY KEY TO CONTINUE";
+    getch();
+    cout<<endl;
+    loading();
+}
+
+int patternGenaration(plane *p)
+{
+    int index;
+    for(int i=0;i<Num;i++)
+    {
+        if(p[i].time>=pattern3[0] && p[i].time<pattern3[1])
+        {
+            p[0].patternOfPlane=pattern[(rand()/7)%3];
+            index=i;
+        }
+        else if(p[i].time>=pattern3[1] && p[i].time<pattern3[2])
+        {
+            p[1].patternOfPlane=pattern[(rand()/7)%3];
+        }
+        else if(p[i].time>=pattern3[2] && p[i].time<pattern3[3])
+        {
+            p[2].patternOfPlane=pattern[(rand()/7)%3];
+        }
+        else if(p[i].time>=pattern3[3] && p[i].time<pattern3[4])
+        {
+            p[3].patternOfPlane=pattern[(rand()/7)%3];
+        }
+        else if(p[i].time>=pattern3[4] && p[i].time<pattern3[5])
+        {
+            p[4].patternOfPlane=pattern[(rand()/7)%3];
+        }
+        else if(p[i].time>=pattern3[5] && p[i].time<pattern3[6])
+        {
+            p[5].patternOfPlane=pattern[(rand()/7)%3];
+        }
+        else if(p[i].time>=pattern3[6] && p[i].time<pattern3[7])
+        {
+            p[6].patternOfPlane=pattern[(rand()/7)%3];
+        }
+        else if(p[i].time>=pattern3[7] && p[i].time<pattern3[8])
+        {
+            p[7].patternOfPlane=pattern[(rand()/7)%3];
+        }
+        else if(p[i].time>=pattern3[8] && p[i].time<pattern3[9])
+        {
+            p[8].patternOfPlane=pattern[(rand()/7)%3];
+        }
+        else if(p[i].time>=pattern3[9])
+        {
+            p[9].patternOfPlane=pattern[(rand()/7)%3];
+        }
+    }
+    return index;
+}
+
+int main()
+{
+    //hum yahan pe kmp ka upyog karenge
+    // bahut saari classes bhi banyenge
+    //har class ek platform represnt karegi
+    //code starts from here
+    intro();
+    system("cls");
+    funfram();
+    system("color 8E");
+    runway r[3];
+    for(int i=0;i<3;i++)
+    {
+        r[i].patternOfRunway=pattern[i];
+    }
+    plane p[Num];
+    string arrival="";
+    p[0].name="SPICE-AF    ";
+    p[1].name="INDIGO-SB   ";
+    p[2].name="GO-NH       ";
+    p[3].name="SPICE-NZ    ";
+    p[4].name="PACIFIC-AA  ";
+    p[5].name="INDIGO-AI   ";
+    p[6].name="AMERICAN-FJ ";
+    p[7].name="DELTA-LD    ";
+    for(int i=0;i<Num;i++)
+    {
+        p[i].time=TimeofPlane[i];
+    }
+    /*p[8].name="CONTINENTAL-SU";
+    p[9].name="KINGFISHER-PX";*/
+    //patternGenaration(p);
+    for(int i=0;i<Num;i++)
+    {
+        p[i].patternOfPlane=pattern[(rand()/7)%3];
+        arrival+=p[i].patternOfPlane;
+    }
+    Function(p,r,arrival);
+}
